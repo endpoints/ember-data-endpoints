68d717d96c06fc2cddf02f1754924bc1e7b0bed7
bmac <bmac325@GMail.com>
2015-04-01 17:56:41 -0400

    Fix bug preventing href relationships from working

--- addon/serializer.js
+++ addon/serializer.js
@@ -7,11 +7,12 @@ var camelize = Ember.String.camelize;
 export default DS.RESTSerializer.extend({
 
   normalize: function(type, hash, prop) {
+    var newLinks = {};
     var links = hash.links;
     for (var key in links) {
       var linkedData = links[key];
       if (linkedData.href) {
-        hash[key] = linkedData.href;
+        newLinks[key] = linkedData.href;
       } else if (linkedData.ids) {
         hash[key] = linkedData.ids;
       } else {
@@ -19,6 +20,8 @@ export default DS.RESTSerializer.extend({
       }
     }
     delete hash.links;
+    hash.links = newLinks;
+
     return this._super(type, hash, prop);
   },
 
