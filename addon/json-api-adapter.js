bd25b20ed77f75d95e655840779603a908db665b
Bob Holt <bobholt@gmail.com>
2015-04-28 11:02:48 -0400

    pluralize type urls

--- addon/json-api-adapter.js
+++ addon/json-api-adapter.js
@@ -108,7 +108,7 @@ export default Adapter.extend(BuildURLMixin, {
 
 
   pathForType: function(typeKey) {
-    return Ember.String.dasherize(typeKey);
+    return Ember.String.pluralize(Ember.String.dasherize(typeKey));
   },
 
 
